f9fe48e28326b80f21d1271ce1539a67d398c20d
Kathy Fan <kathy.z.fan@gmail.com>
2021-04-19 15:44:43 -0700

    add grid search that returns cv_results_

--- utils.py
+++ utils.py
@@ -219,6 +219,63 @@ def fit_classifier_with_hyperparameter_search(
     return crossvalidator.best_estimator_
 
 
+def fit_classifier_with_hyperparameter_search_return_all(
+        X, y, basemod, cv, param_grid, scoring='f1_macro', verbose=True):
+    """
+    Fit a classifier with hyperparameters set via cross-validation.
+
+    Parameters
+    ----------
+    X : 2d np.array
+        The matrix of features, one example per row.
+
+    y : list
+        The list of labels for rows in `X`.
+
+    basemod : an sklearn model class instance
+        This is the basic model-type we'll be optimizing.
+
+    cv : int or an sklearn Splitter
+        Number of cross-validation folds, or the object used to define
+        the splits. For example, where there is a predefeined train/dev
+        split one wants to use, one can feed in a `PredefinedSplitter`
+        instance to use that split during cross-validation.
+
+    param_grid : dict
+        A dict whose keys name appropriate parameters for `basemod` and
+        whose values are lists of values to try.
+
+    scoring : value to optimize for (default: f1_macro)
+        Other options include 'accuracy' and 'f1_micro'. See
+        http://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
+
+    verbose : bool
+        Whether to print some summary information to standard output.
+
+    Prints
+    ------
+    To standard output (if `verbose=True`)
+        The best parameters found.
+        The best macro F1 score obtained.
+
+    Returns
+    -------
+    An instance of the same class as `basemod`.
+        A trained model instance, the best model found.
+
+    """
+    if isinstance(cv, int):
+        cv = StratifiedShuffleSplit(n_splits=cv, test_size=0.20)
+    # Find the best model within param_grid:
+    crossvalidator = GridSearchCV(basemod, param_grid, cv=cv, scoring=scoring)
+    crossvalidator.fit(X, y)
+    # Report some information:
+    if verbose:
+        print("Best params: {}".format(crossvalidator.best_params_))
+        print("Best score: {0:0.03f}".format(crossvalidator.best_score_))
+    # Return the best model found:
+    return crossvalidator.cv_results_, crossvalidator.best_estimator_
+
 def get_vocab(X, n_words=None, mincount=1):
     """
     Get the vocabulary for an RNN example matrix `X`, adding $UNK$ if
